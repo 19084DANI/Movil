318c883eaa4891bee3a79083a90652f8862cdefb
arlocarrillo <124049071@upq.edu.mx>
2025-11-02 14:21:02 -0600

    Update recuperarContrasena.js

---
+++ ProyectoMovil/Screens/recuperarContrasena.js
@@ -0,0 +1,11 @@
+import { Text, StyleSheet, View } from 'react-native'
+
+export default function recuperarContrasena() {
+    return (
+      <View>
+        <Text>recuperarContrasena</Text>
+      </View>
+    )
+}
+
+const styles = StyleSheet.create({});
