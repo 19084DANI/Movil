7183d18230c3869c5634d3cf71c0db8a29cd04f8
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2025-11-02 21:20:58 -0600

    Encabezados para transacciones

--- ProyectoMovil/Screens/TransaccionesScreen.js
+++ ProyectoMovil/Screens/TransaccionesScreen.js
@@ -1,12 +1,19 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, ImageBackground, Image } from 'react-native'
 
 
 export default function TransaccionesScreen() {
 
     return (
-      <View>
-        <Text> Transacciones</Text>
-      </View>
+      <ImageBackground source={require('../assets/fondo1.jpg')} resizeMode='cover'
+             style={styles.backgrounds} >
+
+         <View style={styles.encabezado}>  
+          <Image style={styles.menuhamburgesa} source={require('../assets/menu.png')}></Image>     
+          <Image style={styles.logo} source={require('../assets/logo.jpg')}></Image>  
+
+          </View>
+      </ImageBackground>
+        
     )
   
 }
