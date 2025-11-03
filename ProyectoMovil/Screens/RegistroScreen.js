e4a9b9bf215bf3f59384346d95e16f32acad2d73
124050142 <124050142@upq.edu.mx>
2025-11-02 20:36:42 -0600

    Creamos los campos iniciales

--- ProyectoMovil/Screens/RegistroScreen.js
+++ ProyectoMovil/Screens/RegistroScreen.js
@@ -1,6 +1,12 @@
 import {Text, StyleSheet, View, TextInput, Button, Image, Alert} from 'react-native';
-import { useState } from 'react';
+import { useActionState, useState } from 'react';
 
 export default function RegistroScreen(){
+    const [nombre, setNombre] = useState('');
+    const [correo, setCorreo] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [contrasena, setContrasena] = useState('');
+    
+
     
 }
