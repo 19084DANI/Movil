dc7f21e882791561502b3b5c17001a4b3f90685a
Luis-Guadalupe-Barron-Duran <124049120@upq.edu.mx>
2025-11-02 21:29:48 -0600

    Estilos para el encabezado, utilice los mismos que en la ventana de presupuestos

--- ProyectoMovil/Screens/TransaccionesScreen.js
+++ ProyectoMovil/Screens/TransaccionesScreen.js
@@ -12,8 +12,69 @@ export default function TransaccionesScreen() {
           <Image style={styles.logo} source={require('../assets/logo.jpg')}></Image>  
 
           </View>
+
+         <View style={styles.encabezado2}>
+                    
+        </View>
       </ImageBackground>
         
     )
   
-}
+}const styles = StyleSheet.create({
+
+contenido: {
+  padding:30,
+  paddingBottom:40,
+},
+
+backgrounds: {
+  flex: 1,
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  width: '100%',
+  height: '100%',
+},
+encabezado:{
+  justifyContent:'space-between',
+flexDirection: 'row',
+alignItems: "center",
+backgroundColor: '#E1F5C4',
+padding: 10,
+borderRadius:10,
+marginBottom:0,
+width: '100%',
+height: '10%',
+
+},
+encabezado2:{
+  
+alignItems: "center",
+backgroundColor: '#E1F5C4',
+padding: 10,
+borderRadius:10,
+marginBottom:0,
+width: '100%',
+height: '10%',
+},
+menuhamburgesa:{
+
+width: 35,
+height: 35,
+},
+agregar:{
+
+width: 45,
+height: 45,
+},
+
+
+logo:{
+
+width: 130,
+height: 90,
+borderRadius: 45,
+borderColor: '#f4e45dff',
+borderWidth:5
+},
+
+})
