1290e6b338c74085fa0dfa9e444fffb3c34c8ec3
Monse-Alarcon <124050107@upq.edu.mx>
2025-11-03 23:26:11 -0600

    Formatos para el fondo y el encabezado

--- ProyectoMovil/Screens/HomeScreen.js
+++ ProyectoMovil/Screens/HomeScreen.js
@@ -1,14 +1,70 @@
-import { Text, StyleSheet, View } from 'react-native'
+import { Text, StyleSheet, View, Image, ImageBackground } from 'react-native'
 
 
 export default function BotonesScreen() {
 
     return (
-      <View>
-        <Text>Home </Text>
-      </View>
+    <ImageBackground source={require('../assets/fondo1.jpg')} resizeMode='cover'
+             style={styles.backgrounds} >
+     <View style={styles.encabezado}>  
+         <Image style={styles.menuhamburgesa} source={require('../assets/menu.png')}></Image>     
+         <Image style={styles.logo} source={require('../assets/logo.jpg')}></Image>  
+    </View>
+
+
+    <View style={styles.encabezado2}>
+                        
+    </View>
+    </ImageBackground>
     )
   
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+encabezado:{
+justifyContent:'space-between',
+flexDirection: 'row',
+alignItems: "center",
+backgroundColor: '#E1F5C4',
+padding: 10,
+borderRadius:10,
+marginBottom:0,
+width: '100%',
+height: '10%',
+
+},
+
+menuhamburgesa:{
+
+width: 35,
+height: 35,
+},
+
+logo:{
+
+width: 100,
+height: 70,
+borderRadius: 45,
+borderColor: '#f4e45dff',
+borderWidth:5
+},
+
+backgrounds: {
+  flex: 1,
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  width: '100%',
+  height: '100%',
+},
+
+encabezado2:{
+  
+alignItems: "center",
+backgroundColor: '#E1F5C4',
+padding: 10,
+borderRadius:10,
+marginBottom:0,
+width: '100%',
+height: '10%',
+},
+})
