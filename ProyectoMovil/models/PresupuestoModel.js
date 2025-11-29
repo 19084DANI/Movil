60b925c4fe9213008959d296dd3ce29bd6b9bd5a
124050142 <124050142@upq.edu.mx>
2025-11-29 17:56:32 -0600

    Creamos la funcion para las Categorias

--- ProyectoMovil/models/PresupuestoModel.js
+++ ProyectoMovil/models/PresupuestoModel.js
@@ -5,5 +5,32 @@ class PresupuestoModel {
     this.tableName = 'presupuestos';
   }
 
-  
+  //Función para crear nueva (categoria) presupuesto
+  async create(presupuestoData){
+    try {
+      const db = await DatabaseService.openDB();
+
+      await db.runAsync(
+        `INSERT INTO presupuestos (categoria, monto)
+         VALUES (?, ?)`,
+        [
+          presupuestoData.categoria.trim(),
+          parseFloat(presupuestoData.monto)
+        ]
+      );
+
+      return {
+        success: true,
+        message: 'Presupuesto creado exitosamente'
+      };
+
+    } catch (error) {
+      console.error('Error en PresupuestoModel.create:', error);
+      return {
+        success: false,
+        error: error.message || 'Error al crear el presupuesto'
+      };
+    }
+
+  }
 }
