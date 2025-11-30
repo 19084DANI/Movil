e994301f6adb28dfc0c76cd615e1aaa73aeccc40
124050142 <124050142@upq.edu.mx>
2025-11-29 20:43:42 -0600

    Importamos los componentes necesarios
    Como tambien importamos los archivos que hemos estado creando y los mandamos llamar

--- ProyectoMovil/Screens/PresupuestosScreen.js
+++ ProyectoMovil/Screens/PresupuestosScreen.js
@@ -1,2 +1,10 @@
 import {View, Text, StyleSheet, ScrollView, TouchableOpacity, ImageBackground, Alert} from "react-native";
 import React, { useEffect, useState } from "react";
+import HomeScreen from "./HomeScreen";
+import NuevoPresupuestoScreen from "./NuevoPresupuestoScreen";
+import { Ionicons } from "@expo/vector-icons";
+import PresupuestoController from "../controllers/PresupuestoController";
+
+export default function PresupuestosScreen() {
+  
+}
