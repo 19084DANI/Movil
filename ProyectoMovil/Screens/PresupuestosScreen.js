b4203574b46cfe751ebb02e9b54de6bf5ea2c821
124050142 <124050142@upq.edu.mx>
2025-11-29 20:49:38 -0600

    Creamos dos funciones
    creamos la función para cargar la informacion al iniciar y para poder calcular el total usado

--- ProyectoMovil/Screens/PresupuestosScreen.js
+++ ProyectoMovil/Screens/PresupuestosScreen.js
@@ -6,5 +6,19 @@ import { Ionicons } from "@expo/vector-icons";
 import PresupuestoController from "../controllers/PresupuestoController";
 
 export default function PresupuestosScreen() {
+  const [screen, setScreen] = useState("default");
+  const [presupuestos, setPresupuestos] = useState([]);
+  const [presupuestoGeneral, setPresupuestoGeneral] = useState(15000);
+  const [totalUsado, setTotalUsado] = useState(0);
+
+  // Cargar información al iniciar
+  const cargarPresupuestos = async () => {
+    const lista = await PresupuestoController.obtenerPresupuestos();
+    setPresupuestos(lista);
+
+    // Calcular total usado
+    const total = lista.reduce((acc, p) => acc + parseFloat(p.monto), 0);
+    setTotalUsado(total);
+  };
   
 }
