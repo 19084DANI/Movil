d447d7c7c12a42280e1e73400258ff9d2002265e
124050142 <124050142@upq.edu.mx>
2025-11-29 20:28:57 -0600

    Creamos las vistas de la Interfaz

--- ProyectoMovil/Screens/NuevoPresupuestoScreen.js
+++ ProyectoMovil/Screens/NuevoPresupuestoScreen.js
@@ -6,5 +6,58 @@ export default function NuevoPresupuestoScreen({ navigation }) {
   const [nombre, setNombre] = useState("");
   const [categoria, setCategoria] = useState("");
   const [monto, setMonto] = useState("");
+  const handleGuardar = async () => {
+    const data = {
+      nombre,
+      categoria,
+      monto
+    };
+
+    const result = await PresupuestoController.crearPresupuesto(data);
+
+    if (!result.success){
+        Alert.alert("Error", result.error);
+        return;
+    }
+    Alert.alert("Éxito", "Presupuesto creado correctamente");
+    navigation.goBack();
+  };
+
+  return (
+    <View style={styles.container}>
+
+      <Text style={styles.titulo}>Nuevo Presupuesto</Text>
+
+      <Text style={styles.label}>Nombre del presupuesto</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Ej: Comida, Viajes..."
+        value={nombre}
+        onChangeText={setNombre}
+      />
+
+      <Text style={styles.label}>Categoría</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Ej: Hogar, Transporte, Ocio..."
+        value={categoria}
+        onChangeText={setCategoria}
+      />
+
+      <Text style={styles.label}>Monto asignado</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Ej: 500"
+        value={monto}
+        onChangeText={setMonto}
+        keyboardType="numeric"
+      />
+
+      <TouchableOpacity style={styles.boton} onPress={handleGuardar}>
+        <Text style={styles.botonTexto}>Guardar Presupuesto</Text>
+      </TouchableOpacity>
+
+    </View>
+  );
   
-  }
+}
